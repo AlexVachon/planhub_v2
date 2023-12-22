bdcccceeb516cfc041557aa3e01f43218a6da4cd
alexandre.vachon <alexandre.vachon@brizodata.com>
2023-12-22 15:16:19 -0500

    Modification sidebar (+photo profile, nom, username)

--- src/SideBar.js
+++ src/SideBar.js
@@ -11,16 +11,28 @@ import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial'}}>
+    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar className='bg-black'>
         <CDBSidebarHeader prefix={<i className="fa fa-bars"></i>}>
-          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            <img src='/outline_hub_white_24dp.png' alt='Hub Logo'/>
-          </a>
+          <div className='d-flex align-items-center'>
+            <div>
+              <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+                <img src='/outline_account_circle_white_24dp.png' alt='Hub Logo' className='d-block' style={{ marginRight: '8px' }} />
+              </a>
+            </div>
+            <div className='d-flex flex-column'>
+              <span style={{ maxWidth: '100px', overflow: 'hidden', textOverflow: 'ellipsis' }}>AlexandreVachon</span>
+              <a href='/profile' onMouseEnter={(e) => (e.target.style.color = '#A9A9A9', e.target.style.textDecoration = 'underline')}
+                onMouseLeave={(e) => (e.target.style.color = '#808080', e.target.style.textDecoration = 'none')} style={{ color: '#808080',fontSize: '8px', textDecoration: 'none' }}>
+                @AlexVachon2000
+              </a>
+            </div>
+          </div>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
+
             <NavLink exact='true' to="/" activeclassname="activeClicked">
               <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
             </NavLink>
@@ -46,7 +58,7 @@ const Sidebar = () => {
               padding: '20px 5px',
             }}
           >
-            <img src='/outline_snowshoeing_white_24dp.png' alt='Image Footer'/>
+            <img src='/outline_snowshoeing_white_24dp.png' alt='Image Footer' />
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
