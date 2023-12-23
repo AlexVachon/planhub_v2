eceb81ab88813408fc6e02cd27004706367c296a
alexandre.vachon <alexandre.vachon@brizodata.com>
2023-12-23 16:50:51 -0500

    -variant

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
       <Bootstrap.Navbar
         bg="body-secondary"
         className="bg-gradient"
-        variant="secondary gradient"
       >
         <Bootstrap.Container>
           <Bootstrap.Navbar.Brand href="#home" className="fs-4">PlanHub</Bootstrap.Navbar.Brand>
