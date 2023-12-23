1775402f0781b70895a04c35a5116f6fcc72badb
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-12-22 23:19:57 -0500

    *SideBar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -6,24 +6,57 @@ import {
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom';
+} from "cdbreact";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-      <CDBSidebar className='bg-black'>
+    <div
+      style={{ display: "flex", height: "100vh", overflow: "scroll initial" }}
+    >
+      <CDBSidebar backgroundColor="black">
         <CDBSidebarHeader prefix={<i className="fa fa-bars"></i>}>
-          <div className='d-flex align-items-center'>
+          <div className="d-flex align-items-center">
             <div>
-              <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-                <img src='/outline_account_circle_white_24dp.png' alt='Hub Logo' className='d-block' style={{ marginRight: '8px' }} />
+              <a
+                href="/"
+                className="text-decoration-none"
+                style={{ color: "inherit" }}
+              >
+                <img
+                  src="/outline_account_circle_white_24dp.png"
+                  alt="Hub Logo"
+                  className="d-block"
+                  style={{ marginRight: "8px" }}
+                />
               </a>
             </div>
-            <div className='d-flex flex-column'>
-              <span style={{ maxWidth: '100px', overflow: 'hidden', textOverflow: 'ellipsis' }}>AlexandreVachon</span>
-              <a href='/profile' onMouseEnter={(e) => (e.target.style.color = '#A9A9A9', e.target.style.textDecoration = 'underline')}
-                onMouseLeave={(e) => (e.target.style.color = '#808080', e.target.style.textDecoration = 'none')} style={{ color: '#808080',fontSize: '8px', textDecoration: 'none' }}>
+            <div className="d-flex flex-column">
+              <span
+                style={{
+                  maxWidth: "100px",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                AlexandreVachon
+              </span>
+              <a
+                href="/profile"
+                onMouseEnter={(e) => (
+                  (e.target.style.color = "#A9A9A9"),
+                  (e.target.style.textDecoration = "underline")
+                )}
+                onMouseLeave={(e) => (
+                  (e.target.style.color = "#808080"),
+                  (e.target.style.textDecoration = "none")
+                )}
+                style={{
+                  color: "#808080",
+                  fontSize: "8px",
+                  textDecoration: "none",
+                }}
+              >
                 @AlexVachon2000
               </a>
             </div>
@@ -32,33 +65,46 @@ const Sidebar = () => {
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-
-            <NavLink exact='true' to="/" activeclassname="activeClicked">
+            <NavLink exact="true" to="/">
               <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
             </NavLink>
             {/* <NavLink exact='true' to="/tables" activeclassname="activeClicked">
               <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
             </NavLink> */}
-            <NavLink exact='true' to="/profile" activeclassname="activeClicked">
+            <NavLink exact="true" to="/profile">
               <CDBSidebarMenuItem icon="user">Profile</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact='true' to="/analytics" activeclassname="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
+            <NavLink
+              exact="true"
+              to="/analytics"
+            >
+              <CDBSidebarMenuItem icon="chart-line">
+                Analytics
+              </CDBSidebarMenuItem>
             </NavLink>
 
-            <NavLink exact='true' to="/documentation" target="_blank" activeclassname="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">Documentation</CDBSidebarMenuItem>
+            <NavLink
+              exact="true"
+              to="/documentation"
+              target="_blank"
+            >
+              <CDBSidebarMenuItem icon="exclamation-circle">
+                Documentation
+              </CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }} className='border-dark border-top'>
+        <CDBSidebarFooter
+          style={{ textAlign: "center" }}
+          className="border-dark border-top"
+        >
           <div
             style={{
-              padding: '20px 5px',
+              padding: "20px 5px",
             }}
           >
-            <img src='/outline_snowshoeing_white_24dp.png' alt='Image Footer' />
+            <img src="/outline_snowshoeing_white_24dp.png" alt="Image Footer" />
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
