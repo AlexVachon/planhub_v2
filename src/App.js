edad498c88ebdad8ad60bfd1a34cda449137e8eb
alexandre.vachon <alexandre.vachon@brizodata.com>
2023-12-27 09:29:46 -0500

    Ajout btn logout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as Bootstrap from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import SideBar from "./components/SideBar";
 import DashBoard from "./components/DashBoard";
+import * as Icon from "react-icons/fa"
 
 function App() {
   return (
@@ -12,7 +13,17 @@ function App() {
         <div className="d-flex">
           <SideBar />
           <div className="d-flex flex-column flex-grow-1">
-            <div className="mx-auto" style={{ overflowY: "auto",overflowX: "hidden", maxHeight: "80vh", marginTop: "5rem"}}>
+            <header className="bg-body-tertiary shadow-sm p-4 text-center w-100" >
+              <Bootstrap.Nav  style={{ position: "relative", left: "95%" }} >
+                <Bootstrap.NavItem id="logout-btn">
+                  <Bootstrap.NavLink className="text-body btn btn-outline-secondary shadow-sm">
+                      <Icon.FaSignOutAlt />
+                  </Bootstrap.NavLink>
+                </Bootstrap.NavItem>
+              </Bootstrap.Nav>
+
+            </header>
+            <div className="mx-auto" style={{ overflowY: "auto", overflowX: "hidden", maxHeight: "80vh", marginTop: "5rem" }}>
               <Bootstrap.Container>
                 <Routes>
                   <Route exact="true" path="/" element={<Navigate to="/dashboard" />} />
