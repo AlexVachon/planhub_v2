657a064a1efa731c44a75eade524282fc1032df4
alexandre.vachon <alexandre.vachon@brizodata.com>
2023-12-29 21:55:06 -0500

    format

--- src/components/DBoardComponents/HomePage.js
+++ src/components/DBoardComponents/HomePage.js
@@ -303,7 +303,6 @@ const HomePage = () => {
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
