758297b64b14ab3f0a49482717a283932d77e08d
alexandre.vachon <alexandre.vachon@brizodata.com>
2024-01-02 11:52:55 -0500

    *hover on star, +BoardItem

--- src/components/DBoardComponents/Boards.js
+++ src/components/DBoardComponents/Boards.js
@@ -1,9 +1,43 @@
-import React, { useState } from "react";
-import *  as Icon from "react-icons/fa"
+import React from "react";
+import * as Icon from "react-icons/fa";
 
+const BoardItem = ({ id, title, imageUrl, boardId, onDragStart }) => (
+    <li
+        key={`background-${id}`}
+        style={{
+            margin: "0 2% 2% 0",
+            padding: "0",
+            transform: "translate(0)",
+            backgroundSize: "cover",
+            backgroundImage: `url(${imageUrl})`,
+            borderRadius: "5px",
+            backgroundPosition: "50%",
+        }}
+        className="board mx-2 shadow"
+        draggable
+        onDragStart={(e) => onDragStart(e, boardId)}
+    >
+        <a href="#" type="button" title="Custom Image" className="d-block">
+            <div className="d-flex justify-content-between position-relative flex-column" style={{ height: "80px" }}>
+                <div title={title} dir="auto" className="d-inline-block board-title">
+                    <div>{title}</div>
+                </div>
+                <div className="d-flex justify-content-end" style={{ alignItems: "baseline", flex: "0 0 auto" }}>
+                    <span title={boardId} dir="auto" className="d-block board-id">
+                        {boardId}
+                    </span>
+                    <span className="d-flex align-items-center position-relative ms-2">
+                        <span title="Remove board from Favorites" className="d-block star-icon">
+                            <Icon.FaStar className="w-100" />
+                        </span>
+                    </span>
+                </div>
+            </div>
+        </a>
+    </li>
+);
 
 const Boards = () => {
-
     const backgroundImages = [
         "https://images.unsplash.com/photo-1703016445632-240f009aa738?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w3MDY2fDB8MXxjb2xsZWN0aW9ufDF8MzE3MDk5fHx8fHwyfHwxNzAzNTkzMDE5fA&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=400",
         "https://images.unsplash.com/photo-1703354445674-7c39f58a37ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDY2fDB8MXxjb2xsZWN0aW9ufDJ8MzE3MDk5fHx8fHwyfHwxNzAzNTkzMDE5fA&ixlib=rb-4.0.3&q=80&w=400",
@@ -12,108 +46,79 @@ const Boards = () => {
 
     ]
 
-    const [isStarHovered, setIsStarHovered] = useState(false);
 
-    const removeFromFavorites = (e) => {
+    const handleDragStart = (e, boardId) => {
+        e.dataTransfer.setData("text/plain", "board-item");
+        e.dataTransfer.setData("boardId", boardId);
+        e.currentTarget.id = boardId;
+        e.currentTarget.dataset.boardId = boardId;
+    };
 
-    }
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e, destinationId) => {
+        e.preventDefault();
+        const data = e.dataTransfer.getData("text/plain");
+        const boardId = e.dataTransfer.getData("boardId");
+        const sourceElement = document.getElementById(boardId);
+
+        if (data && boardId == sourceElement.id) {
+            const destinationList = document.getElementById(destinationId);
+            const number = sourceElement.dataset.boardId;
+            sourceElement.id = destinationId.split("-")[0] + `-item-${number}`;
+            const clonedElement = sourceElement.cloneNode(true);
+            destinationList.appendChild(clonedElement);
+            sourceElement.parentNode.removeChild(sourceElement);
+            console.log(`Move and copy the element with ID ${boardId} to ${destinationId}`);
+        }
+    };
 
     return (
         <div>
-            <div className="d-flex align-items-center h6">
-                <Icon.FaRegStar className="me-3" />
-                <div className="fw-bold">Favorites Boards</div>
+            <div>
+                <div className="d-flex align-items-center h6">
+                    <Icon.FaRegStar className="me-3" />
+                    <div className="fw-bold">Favorites Boards</div>
+                </div>
+                <div style={{ height: "fit-content" }}>
+                    <ul
+                        className="d-flex m-0 p-0 board-list"
+                        style={{
+                            flexWrap: "wrap",
+                            justifyContent: "flex-start",
+                            listStyle: "none",
+                        }}
+                        id="favorites-list"
+                        onDragOver={handleDragOver}
+                        onDrop={(e) => handleDrop(e, "favorites-list")}
+                    >
+                        {backgroundImages.map((background, index) => (
+                            <BoardItem
+                                key={index}
+                                id={index}
+                                title={`Mon Projet ${index + 1}`}
+                                imageUrl={background}
+                                boardId={`favorites-item-${index + 1}`}
+                                onDragStart={handleDragStart}
+                            />
+                        ))}
+                    </ul>
+                </div>
             </div>
-            <div style={{ height: "fit-content" }}>
+            <div>
                 <ul
-                    className="d-flex m-0 p-0"
+                    id="myboards-list"
+                    className="d-flex m-0 p-0 board-list"
                     style={{
                         flexWrap: "wrap",
                         justifyContent: "flex-start",
-                        listStyle: "none",
                     }}
-                    id="favorites-board"
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, "myboards-list")}
                 >
-                    <li
-                        key={`background-${0}`}
-                        style={{
-                            margin: "0 2% 2% 0",
-                            padding: "0",
-                            transform: "translate(0)",
-                            backgroundSize: "cover",
-                            backgroundImage: `url(${backgroundImages[0]})`,
-                            borderRadius: "5px",
-                            backgroundPosition: "50%",
-                        }}
-                        className="shadow"
-                    >
-                        <a
-                            href="#"
-                            type="button"
-                            title="Custom Image"
-                            className="d-block"
-                        >
-                            <div
-                                className="d-flex justify-content-between position-relative flex-column"
-                                style={{ height: "80px" }}
-                            >
-                                <div
-                                    title="Mon Projet"
-                                    dir="auto"
-                                    className="d-inline-block board-title"
-                                    style={{
-                                        fontSize: "20px",
-                                        fontWeight: "bold",
-                                        maxHeight: "40px",
-                                        overflow: "hidden",
-                                        width: "100%",
-                                        wordWrap: "break-word",
-                                    }}
-                                >
-                                    <div>Mon Projet</div>
-                                </div>
-                                <div
-                                    className="d-flex justify-content-end"
-                                    style={{
-                                        alignItems: "baseline",
-                                        flex: "0 0 auto",
-                                    }}
-                                >
-                                    <span
-                                        title="Projet-web-react-2023"
-                                        dir="auto"
-                                        className="d-block" // Appliquez la classe ici
-                                        style={{
-                                            flex: "1 1 auto",
-                                            fontWeight: "bold",
-                                            letterSpacing: ".02rem",
-                                            lineHeight: "18px",
-                                            overflow: "hidden",
-                                            textOverflow: "ellipsis",
-                                            whiteSpace: "nowrap",
-                                            width: "100%",
-                                        }}
-                                    >
-                                        Projet-web-react-2023
-                                    </span>
-                                    {/*Je veux qu'il soit bloquer ici */}
-                                    {/* Mais, je veux que  */}
-                                    <span
-                                        className="d-flex align-items-center position-relative"
-                                        style={{ marginLeft: "4px", overflow: "hidden", right: "-4px" }}
-                                    >
-                                        <span
-                                            title="Click here to remove this favorites board. It will be removed from your favorites list."
-                                            className="d-block star-icon"
-                                            style={{ fontSize: "14px", height: "18px" }}
-                                        >
-                                            <Icon.FaStar className="w-100" />
-                                        </span>
-                                    </span>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
+                    <li></li>
                 </ul>
             </div>
         </div>
