dab66cdaf31ea396552b0522b409f7fa077939a6
alexandre.vachon <alexandre.vachon@brizodata.com>
2024-01-02 07:58:34 -0500

    +star remove from favorites

--- src/components/DBoardComponents/Boards.js
+++ src/components/DBoardComponents/Boards.js
@@ -1,5 +1,4 @@
-import React from "react";
-import * as Bootstrap from "react-bootstrap";
+import React, { useState } from "react";
 import *  as Icon from "react-icons/fa"
 
 
@@ -13,57 +12,112 @@ const Boards = () => {
 
     ]
 
+    const [isStarHovered, setIsStarHovered] = useState(false);
+
+    const removeFromFavorites = (e) => {
+
+    }
+
     return (
         <div>
             <div className="d-flex align-items-center h6">
                 <Icon.FaRegStar className="me-3" />
-                <div className="fw-bold">
-                    Favorites Boards
-                </div>
+                <div className="fw-bold">Favorites Boards</div>
             </div>
             <div style={{ height: "fit-content" }}>
-                <ul className="d-flex m-0 p-0" style={{ flexWrap: "wrap", justifyContent: "flex-start", listStyle: "none" }} id="favorites-board">
-                    <li key={`background-${0}`} style={{ margin: "0 2% 2% 0", padding: "0", transform: "translate(0)" }} className="shadow">
+                <ul
+                    className="d-flex m-0 p-0"
+                    style={{
+                        flexWrap: "wrap",
+                        justifyContent: "flex-start",
+                        listStyle: "none",
+                    }}
+                    id="favorites-board"
+                >
+                    <li
+                        key={`background-${0}`}
+                        style={{
+                            margin: "0 2% 2% 0",
+                            padding: "0",
+                            transform: "translate(0)",
+                            backgroundSize: "cover",
+                            backgroundImage: `url(${backgroundImages[0]})`,
+                            borderRadius: "5px",
+                            backgroundPosition: "50%",
+                        }}
+                        className="shadow"
+                    >
                         <a
+                            href="#"
                             type="button"
                             title="Custom Image"
-                            style={{ backgroundSize: "cover", backgroundImage: `url(${backgroundImages[0]})`, borderRadius: "5px", backgroundPosition: "50%"}}
                             className="d-block"
                         >
-                            <div className="d-flex justify-content-between position-relative flex-column" style={{ height: "80px" }}>
-                                <div title="Mon Projet" dir="auto" className="d-inline-block board-title" style={{ flex: "0 0 auto", fontSize: "20px", fontWeight: "bold", maxHeight: "40px", overflow: "hidden", width: "100%", wordWrap: "break-word" }}>
-                                    <div>
-                                        Mon Projet
-                                    </div>
+                            <div
+                                className="d-flex justify-content-between position-relative flex-column"
+                                style={{ height: "80px" }}
+                            >
+                                <div
+                                    title="Mon Projet"
+                                    dir="auto"
+                                    className="d-inline-block board-title"
+                                    style={{
+                                        fontSize: "20px",
+                                        fontWeight: "bold",
+                                        maxHeight: "40px",
+                                        overflow: "hidden",
+                                        width: "100%",
+                                        wordWrap: "break-word",
+                                    }}
+                                >
+                                    <div>Mon Projet</div>
                                 </div>
-                                <div className="d-flex justify-content-end" style={{ alignItems: "baseline", flex: "0 0 auto" }}>
-                                    <span title="Projet-web-react-2023" dir="auto" className="d-block" style={{ flex: "1 1 auto", fontWeight:"bold", letterSpacing: ".02rem", lineHeight: "18px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", width: "100%" }}>
+                                <div
+                                    className="d-flex justify-content-end"
+                                    style={{
+                                        alignItems: "baseline",
+                                        flex: "0 0 auto",
+                                    }}
+                                >
+                                    <span
+                                        title="Projet-web-react-2023"
+                                        dir="auto"
+                                        className="d-block" // Appliquez la classe ici
+                                        style={{
+                                            flex: "1 1 auto",
+                                            fontWeight: "bold",
+                                            letterSpacing: ".02rem",
+                                            lineHeight: "18px",
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            whiteSpace: "nowrap",
+                                            width: "100%",
+                                        }}
+                                    >
                                         Projet-web-react-2023
                                     </span>
-                                    <span className="d-flex align-items-center position-relative" style={{ flex: "0 0 auto", marginLeft: "4px", overflow: "hidden", right: "-4px" }}>
-                                        <span title="Click here to remove this favorites board. It will be removed from your favorites list." className="d-block" style={{ fontSize: "14px", height: "18px", lineHeight: "18px", WebkitTransform: "translateZ(0)", transitionDuration: ".15s", transitionProperty: "color, background, opacity, transform, width, margin" }}>
-                                            <Icon.FaStar color="#FFBF00" />
+                                    {/*Je veux qu'il soit bloquer ici */}
+                                    {/* Mais, je veux que  */}
+                                    <span
+                                        className="d-flex align-items-center position-relative"
+                                        style={{ marginLeft: "4px", overflow: "hidden", right: "-4px" }}
+                                    >
+                                        <span
+                                            title="Click here to remove this favorites board. It will be removed from your favorites list."
+                                            className="d-block star-icon"
+                                            style={{ fontSize: "14px", height: "18px" }}
+                                        >
+                                            <Icon.FaStar className="w-100" />
                                         </span>
                                     </span>
                                 </div>
                             </div>
-                            {/* <div className="p-3">
-                                <div className="h6 fw-bold">
-                                    Mon Projet
-                                </div>
-                                <div className="d-flex align-items-center">
-                                    <div className="me-3">
-                                        Projet-Web-Trop-Cool-2023
-                                    </div>
-                                    <Icon.FaStar color="#FFBF00" />
-                                </div>
-                            </div> */}
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Boards;
